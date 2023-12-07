880369e1367f1db10ec008a1b62efe2eb01d35f0
amingaalex <amingaalex20@gmail.com>
2023-12-07 12:00:22 +0300

    replacing intand string

--- lesson2/stringstorage.dart
+++ lesson2/stringstorage.dart
@@ -1,8 +1,9 @@
 void main() {
 
   String charactername = "Alex";
+  String characterAge = "18";
   print("There was a man named ${charactername}");
-  print("He was 18 yrs old");
+  print("He was ${characterAge} yrs old");
   print("He liked the name ${charactername}");
-  print("But he  liked being 18yrs");
+  print("But he  liked being ${characterAge} yrs old");
 }
