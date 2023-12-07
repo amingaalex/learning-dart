b5299f4fca6ff05059ff8dfc36fd49994f1125fc
amingaalex <amingaalex20@gmail.com>
2023-12-07 12:08:09 +0300

    magnifying variables

---
+++ lesson2/magnifyingvariable.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  String charactername = "Alex";
+  String characterAge = "18";
+  print("There was a man named ${charactername}");
+  print("He was ${characterAge} yrs old");
+
+  charactername = "john";
+  print("He liked the name ${charactername}");
+  print("But he  liked being ${characterAge} yrs old");
+}
