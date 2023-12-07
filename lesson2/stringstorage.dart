3ff8410a8c7cffeba4d1fa833d9e84e66e8f0f64
amingaalex <amingaalex20@gmail.com>
2023-12-07 11:51:48 +0300

    stringstorage

---
+++ lesson2/stringstorage.dart
@@ -0,0 +1,8 @@
+void main() {
+
+  String charactername = "Alex";
+  print("There was a man named ${charactername}");
+  print("He was 18 yrs old");
+  print("He liked the name ${charactername}");
+  print("But he  liked being 18yrs");
+}
