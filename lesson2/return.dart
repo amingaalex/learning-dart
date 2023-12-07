dbd05b5f2ea54d80d519981e2561753d2d1e76b9
amingaalex <amingaalex20@gmail.com>
2023-12-07 18:36:32 +0300

    adding numbers then return

---
+++ lesson2/return.dart
@@ -0,0 +1,13 @@
+double addNumbers(double num1, double num2){
+  print("Before return");
+  return num1 + num2;
+  print("After return");
+  
+}
+void main() {
+
+  double answer = addNumbers(5,10);
+
+  print(answer);
+
+}
