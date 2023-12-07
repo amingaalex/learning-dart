e4226145b3c57d5739e940fa90e7154d0554d39a
amingaalex <amingaalex20@gmail.com>
2023-12-07 14:40:54 +0300

    entering data

---
+++ lesson2/addition.dart
@@ -0,0 +1,9 @@
+
+import "dart:io";
+void main() {
+  print("Enter first number:");
+  String? num1 = stdin.readLineSync();
+  print("Enter second number:");
+  String? num2 = stdin.readLineSync();
+
+}
