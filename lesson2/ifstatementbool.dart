dbafc21a2d4358233a71d106b5c632928f5f533d
amingaalex <amingaalex20@gmail.com>
2023-12-08 13:43:34 +0300

    converting data type

---
+++ lesson2/ifstatementbool.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  bool issmart = false;
+  bool isstudent = false;
+
+  if(issmart && isstudent){
+  print("you are smart a student");
+}else if(issmart && !isstudent){
+  print("you are not smart but a student");
+}else if()
+}
