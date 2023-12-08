f6598e4a7a988236a8a1adde6f81912ee6e9a55a
amingaalex <amingaalex20@gmail.com>
2023-12-08 18:24:58 +0300

    revision on how to use return function

---
+++ lesson2/lesson3-revision/return.dart
@@ -0,0 +1,17 @@
+
+
+void main() {
+  print("greetings");
+  String greet = greeting();
+  int age = getAge();
+  print(age);
+  print(greet);
+}
+
+String greeting() {
+  return "hello";
+}
+
+int getAge() {
+  return 21;
+}
