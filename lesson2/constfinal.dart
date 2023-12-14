92d86d87ba5b53de37bed3a20cb89d8557f1759c
amingaalex <amingaalex20@gmail.com>
2023-12-14 23:48:44 +0300

    using constant and final

---
+++ lesson2/constfinal.dart
@@ -0,0 +1,12 @@
+void main(void) {
+//constant and final
+  const String fullname = "Alex Abere ";
+  final String nickname = "j";
+
+  //declaring and not using
+  var myName;
+  print(myName);
+
+  myName = "John";
+   print(myName);
+}
